48165e3a3e7d3276ae7010bb4ebbfe1d0b6b141a
Hayley Dixon <hdixon37@gmail.com>
2021-11-20 13:24:36 -0700

    Config React App

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+ const [ projects, setProjects ] =useState([])
+ const [ error, setError ] =useState('')
+ const [ isLoading, setIsLoading ] =useState(false)
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
